7c8f76325158c51ba89765bf3d640e5d09c4aa7d
Atonin <aizhansarievam@gmail.com>
2022-02-24 17:48:51 +0600

    first Commit

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -2,13 +2,24 @@ package peaksoft;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import peaksoft.AppConfig;
+import peaksoft.HelloWorld;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean==bean1);
+        System.out.println(bean.equals(bean1));
+
         System.out.println(bean.getMessage());
+
+        Cat bean2= applicationContext.getBean("cat",Cat.class);
+        Cat bean3= applicationContext.getBean("cat",Cat.class);
+        System.out.println(bean2==bean3);
+        System.out.println(bean2.equals(bean3));
+
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -1,14 +1,27 @@
 package peaksoft;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
+  // @ComponentScan("java")
+  // @PropertySource("classpath:application.properties")
 public class AppConfig {
+
     @Bean(name="helloworld")
+
     public HelloWorld getHelloWorld() {
+
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+        @Bean(name="cat")
+     @Scope("prototype")
+     public Cat getCat(){
+
+    Cat cat=new Cat();
+    cat.setName("I am Cat");
+    return cat;
+        }
+
 }
